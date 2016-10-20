7021750fc8841cdf5409c3077b0adb18dd47cb99
menno.van.der.woude@rhdhv.com <menno.van.der.woude@rhdhv.com>
2016-10-20 16:34:59 +0200

    Changed post build event for installer, see comment in App.xaml.cs

--- MiniTD/App.xaml.cs
+++ MiniTD/App.xaml.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+// Note for installer project:
+// added post build event as described here:
+// http://stackoverflow.com/questions/1652301/how-to-sign-installation-files-of-a-visual-studio-msi
+
 namespace MiniTD
 {
     /// <summary>
